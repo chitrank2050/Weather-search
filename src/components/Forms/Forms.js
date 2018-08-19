55fdab765123870bd89286949bcde02d44e2910e
chitrank2050 <chitrank2050@gmail.com>
2018-08-19 10:13:34 +0530

    changed UI components to stateless components

--- src/components/Forms/Forms.js
+++ src/components/Forms/Forms.js
@@ -1,15 +1,11 @@
 import React from 'react';
 
-class Forms extends React.Component {
-    render() {
-        return (
-            <form onSubmit={this.props.getWeather}>
-                <input type="text" name="city" placeholder="City..."/>
-                <input type="text" name="country" placeholder="Country..."/>
-                <button>Get weather</button>
-            </form>
-        );
-    }
-};
+const Forms = props => (
+    <form onSubmit={props.getWeather}>
+        <input type="text" name="city" placeholder="City..."/>
+        <input type="text" name="country" placeholder="Country..."/>
+        <button>Get weather</button>
+    </form>
+);
 
 export default Forms;
